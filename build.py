67a2044b2edcb43ba724e82e43c6773c6f72cb8c
Matej Novota <matej.novota@gmail.com>
2022-02-28 17:30:14 +0100

    organizers: merge build and build_organizers to remove dependences

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import os
 import sys
+import shutil
 from collections import defaultdict, namedtuple
 from datetime import date, datetime
 
@@ -19,10 +20,11 @@ def years_from_school_year(school_year):
     start_year = int(school_year.split("_")[0])
     return (start_year, start_year + 1)
 
-
+logos = ["icon", "icon-darkmode", "small-icon", "small-icon-darkmode"]
 ErrorData = namedtuple("ErrorData", ["file", "message"])
 ERRORS = []
 OUTPUT = defaultdict(lambda: [])
+OUTPUT_ORGANIZERS = {}
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
 parser = argparse.ArgumentParser()
@@ -35,8 +37,32 @@ args = parser.parse_args()
 with open(os.path.join(ROOT, "schemas", "event.schema.json")) as f:
     validate_event = fastjsonschema.compile(json.load(f))
 
-with open(os.path.join(ROOT, "build", "organizers.json")) as f:
-    organizers = json.load(f)
+with open(os.path.join(ROOT, "schemas", "organizer.schema.json")) as f:
+    validate_organizer = fastjsonschema.compile(json.load(f))
+
+print("validating orgnanizers")
+
+for directory in os.walk(os.path.join(ROOT, "organizers")):
+    for file in directory[2]:
+        name, ext = os.path.splitext(file)
+        if ext.lower() not in [".yaml", ".yml"]:
+            continue
+        path = os.path.join(directory[0], file)
+
+        with open(path) as f:
+            organizer_data = yaml.safe_load(f)
+            try:
+                organizer_data = validate_organizer(organizer_data)
+                for logo in logos:
+                    if logo in organizer_data and not os.path.exists(os.path.join(ROOT, "organizers", organizer_data[logo])):
+                        raise JsonSchemaValueException("Invalid path to %s, %s" % (logo, organizer_data[logo]))
+                OUTPUT_ORGANIZERS[name] = organizer_data
+                print(".", end="", flush=True)
+            except JsonSchemaException as e:
+                ERRORS.append(ErrorData(path, e.message))
+                print("F", end="", flush=True)
+
+print("\nValidating events")
 
 for directory in os.walk(os.path.join(ROOT, "data")):
     for file in directory[2]:
@@ -50,7 +76,7 @@ for directory in os.walk(os.path.join(ROOT, "data")):
             try:
                 event_data = validate_event(event_data)
                 for organizer in event_data["organizers"]:
-                    if organizer not in organizers:
+                    if organizer not in OUTPUT_ORGANIZERS:
                         raise JsonSchemaValueException("Organizer %s is not in organizers." % (organizer))
                 if not args.dry:
                     event_date = datetime.strptime(
@@ -71,6 +97,17 @@ if len(ERRORS):
 
 if not args.dry:
     os.makedirs(os.path.join(ROOT, "build"), exist_ok=True)
+    print("Writing output for organizers.")
+
+    for reference, organizer in OUTPUT.items():
+        for logo in logos:
+            if logo in organizer_data:
+                os.makedirs(os.path.dirname(os.path.join(ROOT, "build", organizer_data[logo])), exist_ok=True)
+                shutil.copy(os.path.join(ROOT, "organizers", organizer_data[logo]), os.path.join(ROOT, "build", organizer_data[logo]))
+
+    with open(os.path.join(ROOT, "build/organizers.json"), "w") as f:
+        json.dump(OUTPUT, f)
+
     for year, events in OUTPUT.items():
         print("Writing output for year %s." % (year))
 
